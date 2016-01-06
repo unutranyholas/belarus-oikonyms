52db976103731298885a16c78b1a130ff510f6cd
unutranyholas <unutranyholas@gmail.com>
2016-01-06 23:52:53 +0300

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import normalize from 'normalize.css';
+import styles from 'styles/App.css';
+
+import React from 'react';
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        Test
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
